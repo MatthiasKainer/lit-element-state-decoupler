23145106f5846912cf984fad45cbfe3bf95e00e6
Matthias Kainer <mail@matthias-kainer.de>
2020-06-13 20:38:23 +0200

    Removed no longer needed merge function

--- clone.ts
+++ clone.ts
@@ -14,19 +14,3 @@ export function shallowClone(value: any) {
       return value;
   }
 }
-export function shallowMerge(value: any, mergeWith: any) {
-  const type = typeof value;
-  switch (type) {
-    case "object":
-      if (!value) {
-        return shallowClone(mergeWith);
-      }
-
-      if (value instanceof Date || value instanceof RegExp) {
-        return mergeWith;
-      }
-      return { ...value, ...mergeWith };
-    default:
-      return shallowClone(mergeWith);
-  }
-}
