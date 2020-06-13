7e0c92e9823e1c289ded958d7150b284621d9356
Matthias Kainer <mail@matthias-kainer.de>
2020-06-13 22:52:11 +0200

    Shallow clone arrays nicely

--- clone.test.ts
+++ clone.test.ts
@@ -5,6 +5,8 @@ describe("shallowClone", () => {
         expect(shallowClone("bla")).toBe("bla")
         expect(shallowClone(42)).toBe(42)
         expect(shallowClone(true)).toBe(true)
+        expect(shallowClone(false)).toBe(false)
+        expect(shallowClone(0)).toBe(0)
     })
     it("should correctly not handle dates because damn they are evil", () => {
         const date = new Date(Date.now())
@@ -20,6 +22,11 @@ describe("shallowClone", () => {
     })
     it("should shallow clone an object nicely", () => {
         const obj = { with: true, some: "fields" }
+        expect(shallowClone(obj)).not.toBe(obj)
+        expect(shallowClone(obj)).toEqual(obj)
+    })    
+    it("should shallow clone an array nicely", () => {
+        const obj = ["a", "b"]
         expect(shallowClone(obj)).not.toBe(obj)
         expect(shallowClone(obj)).toEqual(obj)
     })
--- clone.ts
+++ clone.ts
@@ -1,16 +1,4 @@
 export function shallowClone(value: any) {
-  const type = typeof value;
-  switch (type) {
-    case "object":
-      if (!value) {
-        return value;
-      }
-
-      if (value instanceof Date || value instanceof RegExp) {
-        return value;
-      }
-      return { ...value };
-    default:
-      return value;
-  }
+  if (typeof value !== "object" || !value || value instanceof Date || value instanceof RegExp) return value
+  return (Array.isArray(value)) ? [...value] : { ...value }
 }
